01d4dff11a66d53da1f84d3be9c3c4b5a1c41ec3
AndzhurT <andjur38@gmail.com>
2024-05-22 20:25:53 -0400

    Add reset button, add styling to header, add eraser
    You can now use eraser, draw, create divs and reset the canvas

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,33 @@
 
+let assignHover = () => {
+    // assign hover effect over added divs
+    const divs = document.querySelectorAll(".section");
 
+    divs.forEach(div => div.addEventListener("mouseover", setColor));
+}
 
-let createDivs = () => {
+let setColor = (e) => {
+    let target = e.target;
+    if (e.buttons == 1 || e.buttons == 3) {
+        target.classList.add("colored");
+    }
+    else if (e.buttons == 2) {
+        target.classList.remove("colored");
+    }
+}
+
+let getNumberOfDivs = () => {
     let number = parseInt(prompt("Type the number of squares per side from 1 to 100 (e.g., 16 for a 16x16 grid)"));
-    while (number < 1 || number > 100) {
+    while (isNaN(number) || number < 1 || number > 100) {
         number = parseInt(prompt("Please type in the number ranging from 1 to 100."))
     }
+
+    return number;
+}
+
+let createDivs = () => {
     
+    let number = getNumberOfDivs();
     const container = document.querySelector(".container");
 
     const totalNumber = number * number;
@@ -21,26 +42,24 @@ let createDivs = () => {
         div.style.height = divWidthPercentage + "%";
 
         container.appendChild(div);
+        
     }
+    // Assign hover effect over added divs
+    container.addEventListener("mouseover", setColor);
+}
 
-
+let resetDivs = () => {
     const divs = document.querySelectorAll(".section");
-
-    divs.forEach(divs => divs.addEventListener("mouseover", setColor));
+    divs.forEach(div => div.classList.remove("colored"));
 }
 
 
-let btn = document.querySelector("button");
-btn.addEventListener("click", createDivs);
+let btnCreateDivs = document.querySelector(".createDivs");
+btnCreateDivs.addEventListener("click", createDivs);
 
+let btnResetDivs = document.querySelector(".resetDivs");
+btnResetDivs.addEventListener("click", resetDivs);
 
+let mainContainer = document.querySelector(".container");
+mainContainer.addEventListener("click", assignHover);
 
-let setColor = (e) => {
-    let target = e.target;
-    target.classList.add("colored");
-    console.log(target);
-}
-
-
-
-// modify flex:basis based on the amount of divs per side
